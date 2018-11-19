869ba218707186cda7de915f7e9597319d80eb73
bblezek <44982292+bblezek@users.noreply.github.com>
2018-11-19 15:32:31 -0600

    Update addToOrder.js

--- script/addToOrder.js
+++ script/addToOrder.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         var amount = $("#number").val();
         var orderID = $("#orderID").val();
         if(itemID!="" && amount!="" && orderID!="") {
-            confirm("Are you sure you want to add " + amount + " of item " + itemID + " to order" + orderID "?");
+            confirm("Are you sure you want to add " + amount + " of item " + itemID + " to order " + orderID "?");
         }
     });
 });
