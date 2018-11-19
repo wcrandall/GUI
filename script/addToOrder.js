dfad0e73a5018221b1aa5e2a5392d1e02f6ddd9f
bblezek <44982292+bblezek@users.noreply.github.com>
2018-11-19 15:23:20 -0600

    Create addToOrder.js

---
+++ script/addToOrder.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+    $("button").click(function(){
+        var name = $("#product-name").val();
+        var amount = $("#number").val();
+        if(name!="" && amount!="") {
+            confirm("Are you sure you want to add " + amount + " of " + name + " to order?");
+        }
+    });
+});
