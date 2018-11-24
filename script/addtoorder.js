c1b2b363b0d146123e44d517a8b839e13481e50b
bblezek <44982292+bblezek@users.noreply.github.com>
2018-11-23 20:58:45 -0600

    Update addtoorder.js

--- script/addtoorder.js
+++ script/addtoorder.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
         var order = $("#order").val();
         var item = $("#item").val();
         var amount = $("#number").val();
-        if(item!="" && amount!="" && order!="") {
+        if(item!="" && amount!="") {
+            if(order!=""){
                confirm("Are you sure you want to add " + amount + " of item " + item + " to order " order "?");
+            }
         }
     });
 });
