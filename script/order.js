f5e29f54b70cadce3380cab3249b4ef09f4081b8
bblezek <44982292+bblezek@users.noreply.github.com>
2018-11-24 08:56:37 -0600

    Update order.js

--- script/order.js
+++ script/order.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $("button").click(function(){
-        var item = $("#item-id").val();
+        var name = $("#product-name").val();
         var amount = $("#number").val();
         if(name!="" && amount!="") {
-            confirm("Are you sure you want to add " + amount + " " + item + " to order?");
+            confirm("Are you sure you want to add " + amount + " " + name + " to order?");
         }
     });
 });
